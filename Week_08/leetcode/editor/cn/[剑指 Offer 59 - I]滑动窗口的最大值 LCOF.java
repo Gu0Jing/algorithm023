a2662faa1630e21b40a86a6e426a5be1b79af128
guojing3 <guojing3@asiainfo.com>
2021-02-22 16:58:58 +0800

    第八周-每日学习成果+1

---
+++ Week_08/leetcode/editor/cn/[剑指 Offer 59 - I]滑动窗口的最大值 LCOF.java
@@ -0,0 +1,63 @@
+//给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。 
+//
+// 示例: 
+//
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+//输出: [3,3,5,5,6,7] 
+//解释: 
+//
+//  滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7 
+//
+// 
+//
+// 提示： 
+//
+// 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。 
+//
+// 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/ 
+// Related Topics 队列 Sliding Window 
+// 👍 189 👎 0
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length; i++) {
+            //校验入队元素
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offer(nums[i]);
+
+            //校验出队元素是否是最大值
+            if (i >= k) {
+                if (deque.peekFirst() == nums[i - k]) {
+                    deque.pollFirst();
+                }
+            }
+
+            //保存单次窗口滑动结果
+            if (i + 1 - k >= 0) {
+                res[i + 1 - k] = deque.peekFirst();
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
